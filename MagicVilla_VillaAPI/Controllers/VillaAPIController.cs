60bc02d4aad0f60f8d9cf2591928ece78c29f1fe
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-07 15:49:20 +0300

    First End point

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers;
+
+// [Route("api/[controller]")]
+[Route("api/VillaAPI")]
+[ApiController]
+public class VillaAPIController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<VillaDTO> GetVillas()
+    {
+        return VillaStore.villaList;
+    }
+    
+    [HttpGet("id:int")]
+    public VillaDTO GetVilla(int id)
+    {
+        return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+    }
+}
