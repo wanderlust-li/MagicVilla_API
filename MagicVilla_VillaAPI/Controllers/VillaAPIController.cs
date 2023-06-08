a9047946f6b3550e36d584e0adc80e7a89963421
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-08 10:13:15 +0300

    Http Patch in Action

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers;
@@ -68,6 +69,41 @@ public class VillaAPIController : ControllerBase
         if (villa == null)
             return NotFound();
         VillaStore.villaList.Remove(villa);
+        return NoContent();
+    }
+
+    [HttpPut("{id:int}", Name = "UpdateVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDto)
+    {
+        if (villaDto == null || id != villaDto.Id)
+            return BadRequest();
+        
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        villa.Name = villaDto.Name;
+        villa.Sqft = villaDto.Sqft;
+        villa.Occupancy = villaDto.Occupancy;
+
+        return NoContent();
+    }
+    
+    
+    [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO)
+    {
+        if (patchDTO == null || id == 0)
+            return BadRequest();
+        
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        if (villa == null)
+            return BadRequest();
+        patchDTO.ApplyTo(villa, ModelState);
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
         return NoContent();
     }
 }
--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -9,4 +9,8 @@ public class VillaDTO
     [Required]
     [MaxLength(30)]
     public string Name { get; set; }
+    
+    public int Occupancy { get; set; }
+    
+    public int Sqft { get; set; }
 }
