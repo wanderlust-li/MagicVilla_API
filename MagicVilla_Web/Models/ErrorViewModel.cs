1e06a4b12584eb4310c5856013e3ab87ceb0af20
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-13 15:23:38 +0300

    setup MVC Web project

---
+++ MagicVilla_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MagicVilla_Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
