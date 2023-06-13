76205272cbb4dae54a0bc927b2c92a230f4610be
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-13 14:47:10 +0300

    add foreign key

---
+++ MagicVilla_VillaAPI/Migrations/20230613114327_AddForeignKeyToVillaTable.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyToVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VillaId",
+                table: "VillaNumbers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 13, 14, 43, 27, 872, DateTimeKind.Local).AddTicks(4296));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 13, 14, 43, 27, 872, DateTimeKind.Local).AddTicks(4354));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VillaNumbers_VillaId",
+                table: "VillaNumbers",
+                column: "VillaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VillaNumbers_Villas_VillaId",
+                table: "VillaNumbers",
+                column: "VillaId",
+                principalTable: "Villas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VillaNumbers_Villas_VillaId",
+                table: "VillaNumbers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VillaNumbers_VillaId",
+                table: "VillaNumbers");
+
+            migrationBuilder.DropColumn(
+                name: "VillaId",
+                table: "VillaNumbers");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 13, 12, 55, 51, 620, DateTimeKind.Local).AddTicks(9684));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 13, 12, 55, 51, 620, DateTimeKind.Local).AddTicks(9728));
+        }
+    }
+}
--- MagicVilla_VillaAPI/Models/VillaNumber.cs
+++ MagicVilla_VillaAPI/Models/VillaNumber.cs
@@ -9,6 +9,11 @@ public class VillaNumber
     [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
     public int VillaNo { get; set; }
     
+    [ForeignKey("Villa")]
+    public int VillaId { get; set; }
+    
+    public Villa Villa { get; set; }
+    
     public string SpecialDetails { get; set; }
 
     public DateTime CreatedDate { get; set; }
