727b5a5c70ec9682226eddfc5197f869d3685324
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-15 15:12:48 +0300

    villa IService

---
+++ MagicVilla_Web/Services/IServices/IVillaService.cs
@@ -0,0 +1,16 @@
+﻿using MagicVilla_Web.Models.Dto;
+
+namespace MagicVilla_Web.Services.IServices;
+
+public interface IVillaService
+{
+    Task<T> GetAllAsync<T>();
+    
+    Task<T> GetAsync<T>(int id);
+
+    Task<T> CreateAsync<T>(VillaCreateDTO dto);
+    
+    Task<T> UpdateAsync<T>(VillaUpdateDTO dto);
+
+    Task<T> DeleteAsync<T>(int id);
+}
