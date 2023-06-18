1d717f0bc4c5f7cb2452f4c16e9870cb3a2c7a9d
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-18 23:32:28 +0300

    update Villa controller & UI

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -54,4 +54,33 @@ public class VillaController : Controller
         
         return View(model);
     }
+    
+    
+    public async Task<IActionResult> UpdateVilla(int villaId)
+    {
+        var response = await _villaService.GetAsync<APIResponse>(villaId);
+        if (response != null && response.IsSuccess)
+        {
+            VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+            return View(_mapper.Map<VillaUpdateDTO>(model));
+        }
+
+        return NotFound();
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> UpdateVilla(VillaUpdateDTO model)
+    {
+        if (ModelState.IsValid)
+        {
+            var response = await _villaService.UpdateAsync<APIResponse>(model);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(IndexVilla));
+            }
+        }
+        
+        return View(model);
+    }
 }
