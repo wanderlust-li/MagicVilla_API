b2ac888bae41169dabc8c14ac2c37d0ff7fd3f33
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-20 00:11:09 +0300

    add villaNumberController

---
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers;
+
+public class VillaNumberController : Controller
+{
+    private readonly IVillaNumberService _villaNumberService;
+
+    private readonly IMapper _mapper;
+
+    public VillaNumberController(IVillaNumberService villaNumberService, IMapper mapper)
+    {
+        _villaNumberService = villaNumberService;
+        _mapper = mapper;
+    }
+}
