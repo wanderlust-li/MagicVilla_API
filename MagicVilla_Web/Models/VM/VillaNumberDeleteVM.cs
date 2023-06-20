22fc6eede1cf971543ea5c82bb9cee6adbb2ce75
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-21 02:28:09 +0300

    put and delete methods in villa number

---
+++ MagicVilla_Web/Models/VM/VillaNumberDeleteVM.cs
@@ -0,0 +1,18 @@
+﻿using MagicVilla_Web.Models.Dto;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MagicVilla_Web.Models.VM;
+
+public class VillaNumberDeleteVM
+{
+    public VillaNumberDeleteVM()
+    {
+        VillaNumber = new VillaNumberDTO();
+    }
+    
+    public VillaNumberDTO VillaNumber { get; set; }
+    
+    [ValidateNever]
+    public IEnumerable<SelectListItem> VillaList { get; set; }
+}
