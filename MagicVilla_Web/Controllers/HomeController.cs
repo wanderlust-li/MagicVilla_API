69d5962c926853e5af680c2713933e46cd351115
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-20 16:29:44 +0300

    villa home page

--- MagicVilla_Web/Controllers/HomeController.cs
+++ MagicVilla_Web/Controllers/HomeController.cs
@@ -1,31 +1,39 @@
 ﻿using System.Diagnostics;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Newtonsoft.Json;
 
 namespace MagicVilla_Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly IVillaService _villaService;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    private readonly IMapper _mapper;
 
-    public IActionResult Index()
+    public HomeController(IVillaService villaService, IMapper mapper)
     {
-        return View();
+        _villaService = villaService;
+        _mapper = mapper;
     }
+    
+    
+    // GET
+    public async Task<IActionResult> Index()
+    {
+        List<VillaDTO> list = new();
 
-    public IActionResult Privacy()
-    {
-        return View();
+        var response = await _villaService.GetAllAsync<APIResponse>();
+        if (response != null && response.IsSuccess)
+        {
+            list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+        }
+        
+        return View(list);
     }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    
+    
 }
