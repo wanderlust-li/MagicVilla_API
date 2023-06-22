f9ecb6bdec45f052f748664796a41d4e6496f7e1
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-22 13:11:26 +0300

    add models for login and registeration

--- MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -8,7 +8,8 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
-    
+
+    public DbSet<LocalUser> LocalUsers { get; set; }
     public DbSet<Villa> Villas { get; set; }
     public DbSet<VillaNumber> VillaNumbers { get; set; }
 
---
+++ MagicVilla_VillaAPI/Migrations/20230622100743_addUsersToDb.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addUsersToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LocalUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalUsers", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 22, 13, 7, 43, 622, DateTimeKind.Local).AddTicks(7106));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 22, 13, 7, 43, 622, DateTimeKind.Local).AddTicks(7158));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocalUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 20, 17, 39, 52, 535, DateTimeKind.Local).AddTicks(9121));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 6, 20, 17, 39, 52, 535, DateTimeKind.Local).AddTicks(9173));
+        }
+    }
+}
