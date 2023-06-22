2aedd3b323b0be4a9426ab773f6d77f5d26ed8df
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-22 13:29:03 +0300

    user repository - login

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -9,24 +9,52 @@ public class UserRepository : IUserRepository
 {
     private readonly ApplicationDbContext _db;
 
-    public UserRepository(ApplicationDbContext db)
+    private string secretKey;
+
+    public UserRepository(ApplicationDbContext db, IConfiguration configuration)
     {
         _db = db;
+        secretKey = configuration.GetValue<string>("ApiSettings:Secret");
     }
 
 
     public bool IsUniqueUser(string username)
     {
-        throw new NotImplementedException();
+        var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+        if (user == null)
+        {
+            return true;
+        }
+
+        return false;
     }
 
-    public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDto)
+    public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDto)
     {
-        throw new NotImplementedException();
+        var user = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower()
+                                                      && u.Password == loginRequestDto.Password);
+
+        if (user == null)
+        {
+            return null;
+        }
+        
+        
     }
 
-    public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+    public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
     {
-        throw new NotImplementedException();
+        LocalUser user = new LocalUser()
+        {
+            UserName = registerationRequestDTO.UserName,
+            Password = registerationRequestDTO.Password,
+            Name = registerationRequestDTO.Name,
+            Role = registerationRequestDTO.Role
+        };
+
+        _db.LocalUsers.Add(user);
+        await _db.SaveChangesAsync();
+        user.Password = "";
+        return user;
     }
 }
