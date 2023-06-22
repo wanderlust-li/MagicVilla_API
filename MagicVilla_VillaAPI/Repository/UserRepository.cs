9d1dab9bfc7341fe23eefeca96cb8c98797b0ccc
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-22 13:37:19 +0300

    generate Token

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -1,7 +1,11 @@
-﻿using MagicVilla_VillaAPI.Data;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MagicVilla_VillaAPI.Repository;
 
@@ -38,8 +42,29 @@ public class UserRepository : IUserRepository
         {
             return null;
         }
-        
-        
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role)
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        LoginResponseDTO loginResponseDto = new LoginResponseDTO()
+        {
+            Token = tokenHandler.WriteToken(token),
+            User = user
+        };
+
+        return loginResponseDto;
     }
 
     public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
