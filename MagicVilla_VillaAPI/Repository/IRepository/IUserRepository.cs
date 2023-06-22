115b34ecc4a45fc7350fbf91af41cb898192b391
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-22 13:20:26 +0300

    add user repository

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository;
+
+public interface IUserRepository
+{
+    bool IsUniqueUser(string username);
+
+    Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDto);
+
+    Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+}
