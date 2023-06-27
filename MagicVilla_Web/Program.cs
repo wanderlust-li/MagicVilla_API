ebbf028a9531875206c66be76d43ffac6fc7a54f
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-27 21:13:19 +0300

    navBar display

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddScoped<IVillaService, VillaService>();
 builder.Services.AddHttpClient<IVillaNumberService, VillaNumberService>();
 builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddDistributedMemoryCache();
