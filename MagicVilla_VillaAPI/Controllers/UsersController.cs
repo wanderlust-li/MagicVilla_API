7ae36a6f5181e9739927ae7cbfea481c62e96c0b
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-27 10:55:55 +0300

    add auth service

--- MagicVilla_VillaAPI/Controllers/UsersController.cs
+++ MagicVilla_VillaAPI/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers;
 
-[Route("api/usersAuth")]
+[Route("api/UsersAuth")]
 [ApiController]
 public class UsersController : Controller
 {
